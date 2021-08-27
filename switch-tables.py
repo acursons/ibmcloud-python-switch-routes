7ff812ec300748311ea4efa65331708237f775a9
Andrew Cursons <andrew.cursons@uk.ibm.com>
2021-08-27 13:06:11 +0100

    parallel subnets

--- switch-tables.py
+++ switch-tables.py
@@ -6,29 +6,23 @@ from ibm_cloud_sdk_core import ApiException
 from multiprocessing import Process
 import os
 import time
-import sys
 import config
 
 # Configuration values
-vpc_name = sys.argv[1]
-failover_subnets = []
-#failover_subnets = [{'primary_route_name': "vpn-route",
-#					 'secondary_route_name': "direct-link-and-vpn",
-#					 'target_host': "193.38.60.239"
-#					},
-#					{'primary_route_name': "new-test",
-#					 'secondary_route_name': "vpn-route",
-#					 'target_host': "8.8.8.8"
-#					}
-#				   ]
+vpc_name = "vpn-test-temp"
+failover_subnets = [{'sn_name': "vpn-failover-test-subnet",
+					 'primary_route_name': "vpn-route",
+					 'secondary_route_name': "direct-link-and-vpn",
+					 'target_host': "193.38.60.239"
+					},
+					{'sn_name': "vpn-test-sub2",
+					 'primary_route_name': "new-test",
+					 'secondary_route_name': "vpn-route",
+					 'target_host': "8.8.8.8"
+					}
+				   ]
 
-class SnItem:
-	def __init__(self, primary_table, secondary_table, trigger):
-		self.primary_route_name = primary_table
-		self.secondary_route_name = secondary_table
-		self.target_host = trigger
-
-def switch_table(vpc_nm, sn_info):
+def monitor_process(vpc_nm, sn_info):
 	
 	authenticator = IAMAuthenticator(config.api_key)
 	service = VpcV1(authenticator=authenticator)
@@ -43,58 +37,97 @@ def switch_table(vpc_nm, sn_info):
 	vpc_id = ''
 	for vpc in vpc_collection:
 		print(vpc['name'], vpc['id'])
-		if vpc['name'] == vpc_nm:
+		if vpc['name'] == vpc_nm:remote-monitoring.py
 			vpc_id = vpc['id']
 	# TODO FAILURE CODE
 
-	# Build a map of routing tables available
+	# Get the IDs for the two routing tables
 	try:
 		route_tables = service.list_vpc_routing_tables(vpc_id).get_result()['routing_tables']
 	except ApiException as e:
 		print("list_routing_tables() failed with status code " + str(e.code) + ": " + e.message)
 
-	route_table_map = {}
+	primary_table_id = ''
+	secondary_table_id = ''
 	for route_table in route_tables:
-		route_table_map[route_table['name']] = route_table['id']
-	print(route_table_map)
+		if route_table['name'] == sn_info['primary_route_name']:
+			primary_table_id = route_table['id']
+		if route_table['name'] == sn_info['secondary_route_name']:
+			secondary_table_id = route_table['id']
+	print("Obtained tables " + primary_table_id + ", " + secondary_table_id)
 
-	# Get the list of subnets for the primary table
-	primary_table_name = sn_info.primary_route_name
-	primary_route_id = route_table_map[primary_table_name]
-	print(primary_route_id)
-	secondary_table_name = sn_info.secondary_route_name
-	secondary_table_id = route_table_map[secondary_table_name]
-	print(secondary_table_id)
-	
+	# Determine which routing table is currently in use
+	using_primary_table = False
 	try:
-		attached_subnet_list = service.get_vpc_routing_table(vpc_id, primary_route_id).get_result()['subnets']
+		subnet_collection = service.list_subnets().get_result()['subnets']
 	except ApiException as e:
-		print("get_vpc_routing_table(): failed with status code " + str(e.code) + ": " + e.message)
-	print(attached_subnet_list)
-	
-	# Switch all subnets to the alternate table
-	routing_table_identity_model = {
-		'id': secondary_table_id,
-	}
+		print("list_subnets() failed with status code " + str(e.code) + ": "+ e.message)
 
-	for subnet in attached_subnet_list:
-		try:
-			response = service.replace_subnet_routing_table(
-				id=subnet['id'],
-				routing_table_identity=routing_table_identity_model
-			).get_result()
-		except ApiException as e:
-			print("replace_subnet_routing_table() failed with status code " + str(e.code) + ": "+ e.message)
-		
+	subnet_id = ''
+	for subnet in subnet_collection:
+		if subnet['name'] == sn_info['sn_name']:
+			subnet_id = subnet['id']
+			try:
+				table_in_use = service.get_subnet_routing_table(subnet['id']).get_result()['id']
+			except ApiException as e:
+				print("get_subnet_routing_table() failed with status code " + str(e.code) + ": " + e.message)
+			using_primary_table = table_in_use == primary_table_id
+
+	while 1:
+
+		#we set success_count to 0
+		success_count = 0
+
+		#here we check if ping is successful and collect statistics
+		for count in range(0, 4):
+			response = os.system("ping -c 1 " + sn_info['target_host'])
+			if response == 0:
+				success_count +=1
+			else:
+				success_count -=1
+
+		if success_count < 4:
+			print("HOST is DOWN")
+			if using_primary_table:
+				routing_table_identity_model = {
+					'id': secondary_table_id,
+				}
+
+				try:
+					response = service.replace_subnet_routing_table(
+						id=subnet_id,
+						routing_table_identity=routing_table_identity_model
+					).get_result()
+				except ApiException as e:
+					print("replace_subnet_routing_table() failed with status code " + str(e.code) + ": "+ e.message)
+				using_primary_table = False
+			
+		else:
+			print("HOST IS UP")
+			if not using_primary_table:
+				routing_table_identity_model = {
+					'id': primary_table_id,
+				}
+
+				try:
+					response = service.replace_subnet_routing_table(
+						id=subnet_id,
+						routing_table_identity=routing_table_identity_model
+					).get_result()
+				except ApiException as e:
+					print("replace_subnet_routing_table() failed with status code " + str(e.code) + ": "+ e.message)
+				using_primary_table = True
+
+	
+		#we are sleeping 5 sec before new cycle
+		time.sleep(5)
 		
 if __name__ == '__main__':		
-	# Get the name of the subnets to switch primary, secondary_route_name
-	failover_subnets.append(SnItem(sys.argv[2], sys.argv[3], "8.8.8.8"))
+	# Get the ID of the VPC
 	
 	#Start a process to handle each subnet
 	for sn in failover_subnets:
-	#	Process(target=monitor_process, args=(vpc_name, sn)).start()
-		result = switch_table(vpc_name, sn)
+		Process(target=monitor_process, args=(vpc_name, sn)).start()
 
 
 
